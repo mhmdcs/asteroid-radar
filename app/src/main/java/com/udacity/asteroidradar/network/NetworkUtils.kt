34eaa5fb33ff6ee307fcd12aadd438de83d8992b
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-04 20:11:38 +0300

    restrcture code round 2

--- app/src/main/java/com/udacity/asteroidradar/network/NetworkUtils.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/NetworkUtils.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar.api
+package com.udacity.asteroidradar.network
 
 import com.udacity.asteroidradar.domain.Asteroid
 import com.udacity.asteroidradar.utils.Constants
