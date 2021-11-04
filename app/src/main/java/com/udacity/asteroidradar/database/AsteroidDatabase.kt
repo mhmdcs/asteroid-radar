deff6bd9de08b894d1e57917491f4239d784bba7
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-04 19:23:01 +0300

    implement a more optimized way to initialize room database

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabase.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabase.kt
@@ -10,32 +10,54 @@ abstract class AsteroidDatabase: RoomDatabase() {
 
     abstract val asteroidDao: AsteroidDao
 
-//    companion object {
-//        private lateinit var INSTANCE: AsteroidDatabase
-//
-//        fun getDatabase(context: Context): AsteroidDatabase {
-//            synchronized(this){
-//                if(!::INSTANCE.isInitialized){//if instance of the database is null, then create it
-//                    INSTANCE = Room
-//                        .databaseBuilder(context.applicationContext, AsteroidDatabase::class.java,"asteroids")
-//                        .build()
-//                }
-//            }
-//            return INSTANCE
-//        }
-//    }
+    //to prevent multiple instances being created, initialize the database in a singleton (companion object)
+    //if you don't synchronise here, then two different threads could both create a new instance of the database
+    // whereas the singleton pattern is supposed to facilitate a single shared instance across the lifecycle of the program
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AsteroidDatabase? = null
+
+        @JvmStatic
+        fun getDatabase(context: Context): AsteroidDatabase {
+            val localInstance = INSTANCE
+
+            if (localInstance != null) {
+                return localInstance
+            }
+
+            return synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AsteroidDatabase::class.java,
+                        "asteroids"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+
+                instance
+            }
+        }
+    }
 
 }
 
-    private lateinit var INSTANCE: AsteroidDatabase
 
-    fun getDatabase(context: Context): AsteroidDatabase {
-        synchronized(AsteroidDatabase::class.java){
-            if(!::INSTANCE.isInitialized){//if instance of the database is null, then create it
-            INSTANCE = Room
-                .databaseBuilder(context.applicationContext, AsteroidDatabase::class.java,"asteroids")
-                .build()
-            }
-        }
-        return INSTANCE
-    }
+    //this implementation of database initialization below is not thread-safe
+    //it is recommended for the database initialization to be a singleton (companion object) due to expensive initialisation cost with synchronization
+//    private lateinit var INSTANCE: AsteroidDatabase
+//
+//    fun getDatabase(context: Context): AsteroidDatabase {
+//        synchronized(AsteroidDatabase::class.java){
+//            if(!::INSTANCE.isInitialized){//if instance of the database is null, then create it
+//            INSTANCE = Room
+//                .databaseBuilder(context.applicationContext, AsteroidDatabase::class.java,"asteroids")
+//                .build()
+//            }
+//        }
+//        return INSTANCE
+//    }
