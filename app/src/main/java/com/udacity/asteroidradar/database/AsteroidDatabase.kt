53266628eb33245a498b1c98b8cfdbcbbb7e7fa7
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-05 15:37:23 +0300

    fix build gradle error by increasing database version number

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabase.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [AsteroidEntity::class, PictureOfDayEntity::class], version = 1)
+@Database(entities = [AsteroidEntity::class, PictureOfDayEntity::class], version = 2)
 abstract class AsteroidDatabase: RoomDatabase() {
 
     abstract val asteroidDao: AsteroidDao
