44a2ebe7ad9f61e2f6ba7ae7d5df694fc82595c6
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-05 15:01:22 +0300

    implement MainFragment

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,9 +3,13 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import com.udacity.asteroidradar.utils.SortFilter
 
 class MainFragment : Fragment() {
 
@@ -20,6 +24,18 @@ class MainFragment : Fragment() {
 
         binding.viewModel = viewModel
 
+        binding.asteroidRecycler.adapter = AsteroidsListAdapter(AsteroidClickListener {
+            viewModel.asteroidClicked(it)
+        })
+
+        viewModel.navigateToDetailsFragment.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                val action = MainFragmentDirections.actionShowDetail(it)
+                NavHostFragment.findNavController(this).navigate(action)
+                viewModel.doneNavigation()
+            }
+        })
+
         setHasOptionsMenu(true)
 
         return binding.root
@@ -31,6 +47,11 @@ class MainFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.show_cached -> viewModel.onChangeSortFilter(SortFilter.CACHED)
+            R.id.show_weekly -> viewModel.onChangeSortFilter(SortFilter.WEEKLY)
+            R.id.show_daily -> viewModel.onChangeSortFilter(SortFilter.TODAY)
+        }
         return true
     }
 }
