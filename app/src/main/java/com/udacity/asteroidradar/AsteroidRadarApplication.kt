81159aadadfd7c297cd19cb97b1c2d087708a5c9
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-05 15:50:09 +0300

    implement worker

--- app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -1,6 +1,56 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.work.*
+import com.udacity.asteroidradar.work.RefreshAsteroidDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class AsteroidRadarApplication: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupWork()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setupWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+
+        val request = PeriodicWorkRequestBuilder<RefreshAsteroidDataWorker>(
+            1,
+            TimeUnit.DAYS
+        ).setConstraints(constraints).build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshAsteroidDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            request
+        )
+    }
+
 }
--- app/src/main/java/com/udacity/asteroidradar/work/RefreshAsteroidDataWorker.kt
+++ app/src/main/java/com/udacity/asteroidradar/work/RefreshAsteroidDataWorker.kt
@@ -1,4 +1,28 @@
 package com.udacity.asteroidradar.work
 
-class RefreshAsteroidDataWorker {
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.database.AsteroidDatabase.Companion.getDatabase
+import com.udacity.asteroidradar.repository.AsteroidRadarRepository
+import retrofit2.HttpException
+
+class RefreshAsteroidDataWorker(appContext: Context, params: WorkerParameters): CoroutineWorker(appContext, params) {
+    companion object {
+        const val WORK_NAME = "RefreshAsteroidsDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val asteroidRepo = AsteroidRadarRepository(database)
+
+        return try {
+            asteroidRepo.refreshPictureOfDay()
+            asteroidRepo.refreshAsteroids()
+
+            Result.success()
+        } catch (exc: HttpException) {
+            Result.retry()
+        }
+    }
 }
